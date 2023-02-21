59537fb2a73bebfdc31760dc8d5e2a519c54cf6b
alaaashraf355138 <alaaashraf2171999@gmail.com>
2023-02-21 06:06:53 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,10 +7,13 @@
 int print_sign(int c)
 {
 	if (c > 0)
-		return (+1);
+		_putchar('+');
+		return (1);
 	else if (c < 0)
-		return (-1);
+		_putchar('-');
+		return (1);
 	else
-		return (00);
+		_putchar('0);
+		return (0);
 
 }
