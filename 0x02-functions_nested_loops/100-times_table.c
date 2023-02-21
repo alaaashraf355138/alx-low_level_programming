3186e6107d8871ac40499bbfb94a79bd189e02bb
alaaashraf355138 <alaaashraf2171999@gmail.com>
2023-02-21 13:16:56 -0800

    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

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,20 +7,20 @@
 void print_times_table(int n)
 {
 	int num, mult, prod;
-	if (n<15)
-	{	
+
+	if (n < 15)
+	{
 		for (num = 0; num <= n; num++)
 		{
-			_putchar('0');
-		
-			for (mult = 1; mult <= 9; mult++)
-			{	
+			_putchar('0');	
+			for (mult = 1; mult <= n; mult++)
+			{
 				_putchar(',');
 				_putchar(' ');
 
 				prod = num * mult;
 
-				if (prod <= 9)
+				if (prod <= n)
 					_putchar(' ');
 				else
 					_putchar((prod / 10) + '0');
