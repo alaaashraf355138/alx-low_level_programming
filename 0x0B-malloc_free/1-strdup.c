8a65911e9e6a4faf9831d4c5b836c27e2173269d
alaaashraf355138 <alaaashraf2171999@gmail.com>
2023-03-13 12:11:47 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,13 +2,15 @@
 /**
  *_strdup - which contains a copy of the string given as a parameter.
  * @str:string to have copy of it
- * Return:copied string
  */
 char *_strdup(char *str)
 {
+int size;
+int pos;
+
+
 if (str == NULL)
 return (NULL);
-int size;
 size = 0;
 while (*(str + size) != '\0')
 {
@@ -17,7 +19,6 @@ size++;
 char *target;
 target = malloc(sizeof(char) * size);
 
-int pos;
 pos = 0;
 while (pos < size)
 {
