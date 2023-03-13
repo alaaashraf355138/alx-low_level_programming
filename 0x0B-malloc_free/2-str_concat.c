6c5c6cac12120e8c1780b8b68d075d8b663056fd
alaaashraf355138 <alaaashraf2171999@gmail.com>
2023-03-13 12:34:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,62 @@
 #include "main.h"
 /**
- *str_concat- back a pointer to array
- * @s1:first string
- * @s2:swcond string
- * Return: two strings together
+ *_strlen - count arrray
+ *@s: array of elements
+ *Return: i
  */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0') /*Count character of string*/
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ *str_concat - back a pointer to array
+ *@s1: Array one
+ *@s2: Array two
+ *Return: Always an array dinamic
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-int siz1;
-int siz2;
-int tot;
-char *target;
-int pos;
-int pos2;
+	char *dst;
+	unsigned int i, j, size;
 
+	/*If the array is empty*/
+	if (s1 == NULL)
+		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
 
+	/*count size total*/
+	size = (_strlen(s1) + _strlen(s2) + 1);
 
-siz1 = 0;
-siz2 = 0;
+	/*malloc*/
+	dst = (char *) malloc(size * sizeof(char));
 
-while (*(s1 + siz1) != '\0')
-{
-siz1++;
-}
-while (*(s2 + siz2) != '\0')
-{
-siz2++;
-}
-tot = siz1 + siz2;
+	if (dst == 0)
+	{
+		return (NULL);
+	}
 
-target = malloc(sizeof(char) * tot);
-pos = 0;
-while (pos < siz1)
-{
-target[pos] = s1[pos];
-pos++;
-}
-pos2 = 0;
-while (siz1 < tot)
-{
-target[siz1] = s2[pos2];
-siz1++;
-pos2++;
-}
-return (target);
+	/*Concatenate arrays*/
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(dst + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dst + i) = *(s2 + j);
+		i++;
+	}
+
+	return (dst);
 }
